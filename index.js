79f5d298f07d20edad9a408b97322de87f88f7a1
shanzaehtisham <shanza.ehtisham@gmail.com>
2017-03-04 17:58:30 +0500

    after person collection

--- index.js
+++ index.js
@@ -17,32 +17,23 @@ MongoClient.connect("mongodb://shanzadb:Indushospital1@ds058369.mlab.com:58369/f
      if(err) throw err;
 
      //Write databse Insert/Update/Query code here..
-	 db.createCollection('requests', function(err, collection){
-		  
-		db.collection('requests', function(err, collection){
-		  var requestCollection = collection;
-		  connect(
-			connect.favicon(),                    // Return generic favicon
-			connect.query(),                      // populate req.query with query parameters
-			connect.bodyParser(),                 // Get JSON data from body
-			function(req, res, next){             // Handle the request
-			  res.setHeader("Content-Type", "application/json");
-			  if(req.query != null) {
-				requestCollection.insert(req.query, function(error, result){
-				  // result will have the object written to the db so let's just
-				  // write it back out to the browser
-				  res.write(JSON.stringify(result));
-				});
-			  }
-			  
-			  res.end();
-			}
-		  ).listen(process.env.PORT || 8080);
-		  // the PORT variable will be assigned by Heroku
-		});
+	 db.collection('Persons', function 	 (err, collection) {
+        
+        collection.insert({ id: 1, firstName: 'Steve', lastName: 'Jobs' });
+        collection.insert({ id: 2, firstName: 'Bill', lastName: 'Gates' });
+        collection.insert({ id: 3, firstName: 'James', lastName: 'Bond' });
+        
+        
+
+        db.collection('Persons').count(function (err, count) {
+            if (err) throw err;
+            
+            console.log('Total Rows: ' + count);
+        });
+    });
+                
 	 
                 
-});
 });
 
 /*
