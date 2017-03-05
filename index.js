cf6584852b68558091ce06052749ffbf59207897
shanzaehtisham <shanza.ehtisham@gmail.com>
2017-03-05 14:18:21 +0500

    post changed

--- index.js
+++ index.js
@@ -12,11 +12,31 @@ var connect = require('connect'),
 	  res.send("Hello world");	  
   });
 
-  /*
+  
   app.post('/', function (req, res) {
 	  res.send("Hello world");
+	  
+	  MongoClient.connect(uri, function (err, db) {
+   
+		 if(err) throw err;
+
+		 //Write databse Insert/Update/Query code here..
+		 db.collection('Persons', function 	 (err, collection) {
+			
+			collection.insert({ id: 1, firstName: 'Steve', lastName: 'Jobs' });
+			collection.insert({ id: 2, firstName: 'Bill', lastName: 'Gates' });
+			collection.insert({ id: 3, firstName: 'James', lastName: 'Bond' });
+			
+			db.collection('Persons').count(function (err, count) {
+				if (err) throw err;
+				
+				console.log('Total Rows: ' + count);
+			});
+		});
+                
+	});
   });
-  */
+  
   
   app.listen(process.env.PORT, function(){
 	  console.log("Listening on port"+process.env.PORT);	  
@@ -25,25 +45,7 @@ var connect = require('connect'),
   
 
 // Connect to the db
-MongoClient.connect(uri, function (err, db) {
-   
-     if(err) throw err;
 
-     //Write databse Insert/Update/Query code here..
-	 db.collection('Persons', function 	 (err, collection) {
-        
-        collection.insert({ id: 1, firstName: 'Steve', lastName: 'Jobs' });
-        collection.insert({ id: 2, firstName: 'Bill', lastName: 'Gates' });
-        collection.insert({ id: 3, firstName: 'James', lastName: 'Bond' });
-		
-        db.collection('Persons').count(function (err, count) {
-            if (err) throw err;
-            
-            console.log('Total Rows: ' + count);
-        });
-    });
-                
-});
 
 
 
