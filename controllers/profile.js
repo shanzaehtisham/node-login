cfb62c480a82364dc33d437a5b08bdf5b9b20ecb
shanzaehtisham <shanza.ehtisham@gmail.com>
2017-03-13 20:08:41 +0500

    mongoose

--- controllers/profile.js
+++ controllers/profile.js
@@ -21,7 +21,7 @@ exports.findAll = function(req, res) {
 
 exports.add = function(req, res) {
 	
-	console.log('JSON Received as : ' +req.body.data);
+	console.log('JSON Received as : ' +req.data);
 	  
 	  MongoClient.connect(uri, function (err, db) {
    
