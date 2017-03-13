b9f58585d4041b5bec134b6e4058bd84385f0813
shanzaehtisham <shanza.ehtisham@gmail.com>
2017-03-13 19:46:32 +0500

    hehe

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ var connect = require('connect'),
   
   require('./routes.js')(app);
   require('./models/profile');
+  app.use(bodyparser.json());
+  app.use(bodyparser.urlencoded({extended: true}));
   
   var uri = "mongodb://shanzadb:Indushospital1@ds058369.mlab.com:58369/fyp";
   var MongoClient = require('mongodb').MongoClient;
   
-  
-  app.use(bodyparser.json());
-
   app.get('/', function (req, res) {
 	  res.send("Hello People");	  
   });  
