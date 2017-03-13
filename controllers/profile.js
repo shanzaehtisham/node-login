c3bf2265b1f6b81662e8e6e6c0944cf0a51dff13
shanzaehtisham <shanza.ehtisham@gmail.com>
2017-03-13 20:04:14 +0500

    mongoose

--- controllers/profile.js
+++ controllers/profile.js
@@ -27,14 +27,29 @@ exports.add = function(req, res) {
    
 		 if(err) throw err;
 
+		 /*
 		 db.collection('profile').insert(req.body, function(err, result) {
 			 if(err)
 					res.send("Error");
 				else
 					res.send("Success");
 		 });
-			//collection.insert({ id: 2, firstName: 'Bill', lastName: 'Gates' });
-
+			*/
+		
+		// previous code
+		
+			db.collection('profile', function(err, collection) {
+				
+				collection.insert(req.body, function(err, result){
+					
+					if(err)
+						res.send("Error");
+					else
+						res.send("Success");
+				});
+			});
+		
+		
 			db.collection('profile').count(function (err, count) {
 				if (err) throw err;
 				
