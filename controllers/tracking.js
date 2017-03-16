98c33bd658f61f3719ee98d9664976ab5b40119b
shanzaehtisham <shanza.ehtisham@gmail.com>
2017-03-16 20:03:59 +0500

    single qoutes

--- controllers/tracking.js
+++ controllers/tracking.js
@@ -54,7 +54,7 @@ exports.updateEndLocation = function(req, res) {
 			function(err, result){
 				console.log('success');
 				if (err) throw err;
-				console.log('result of update = '+ json.stringify(result));
+				console.log('result of update = '+ JSON.stringify(result));
 			}
 		
 		);
