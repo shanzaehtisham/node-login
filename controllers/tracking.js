4b603d570c4ac15836fafb56a78d44f3f087d066
shanzaehtisham <shanza.ehtisham@gmail.com>
2017-03-16 17:55:36 +0500

    single qoutes

--- controllers/tracking.js
+++ controllers/tracking.js
@@ -47,9 +47,11 @@ exports.updateEndLocation = function(req, res) {
 			},
 			{ 
 				"$set": 
-					{"End_location_latlng": req.body.End_location_latlng},
-					{"End_location_name": req.body.End_location_name},
-					{"Journey_EndDateTime" : req.body.Journey_EndDateTime}
+					{
+						"End_location_latlng": req.body.End_location_latlng,
+						"End_location_name": req.body.End_location_name,
+						"Journey_EndDateTime" : req.body.Journey_EndDateTime
+					}
 			}
 			{	upsert:false,
 				multi:false
